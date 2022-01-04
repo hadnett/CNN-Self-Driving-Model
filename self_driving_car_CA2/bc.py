30c12d094bc731053db3a88d2d930c6dd6a68c03
hadnett <williamsrhadnett@outlook.com>
2022-01-04 19:44:07 +0000

    Analysed and processed throttle data
    Analysed the throttle data and as expected there was a lot of
    one values contained within the data. These one values were
    reduced to prevent a basis towards full throttle.
    
    Removing these values reduced the dataset from 1455 to 852.
    
    Note additional analysis will be required as the dataset grows,
    currently working with a small amount of data to setup and
    validate the code is working.

--- self_driving_car_CA2/bc.py
+++ self_driving_car_CA2/bc.py
@@ -5,7 +5,6 @@ from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 def path_leaf(path):
     head, tail = ntpath.split(path)
     return tail
@@ -20,6 +19,8 @@ print(data.head())
 data['center'] = data['center'].apply(path_leaf)
 data['left'] = data['left'].apply(path_leaf)
 data['right'] = data['right'].apply(path_leaf)
+
+# Steering Data
 
 num_bins = 25
 samples_per_bin = 300
@@ -36,7 +37,7 @@ for j in range(num_bins):
     for i in range(len(data['steering'])):
         if bins[j] <= data['steering'][i] <= bins[j + 1]:
             list_.append(i)
-    list_ = shuffle(list_)
+    list_ = shuffle(list_, random_state=0)
     list_ = list_[samples_per_bin:]
     remove_list.extend(list_)
 
@@ -51,3 +52,37 @@ plt.show()
 
 print(data.iloc[1])
 
+# Throttle Data
+
+num_bins = 5
+samples_per_bin = 420
+hist, bins = np.histogram(data['throttle'], num_bins)
+print(bins)
+center = (bins[:-1] + bins[1:])*0.5
+plt.bar(center, hist, width=0.05)
+plt.plot((np.min(data['throttle']), np.max(data['throttle'])), (samples_per_bin, samples_per_bin))
+plt.show()
+
+# Indexes where changed after previous delete. Therefore, they needed to be reset in order to preform
+# an additional delete. https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.reset_index.html
+remove_list = []
+data = data.reset_index(drop=True)
+for j in range(num_bins):
+    list_ = []
+    for i in range(len(data['throttle'])):
+        if bins[j] <= data['throttle'][i] <= bins[j + 1]:
+            list_.append(i)
+    list_ = shuffle(list_)
+    list_ = list_[samples_per_bin:]
+    remove_list.extend(list_)
+
+print('Remove: ', len(remove_list))
+data.drop(data.index[remove_list], inplace=True)
+print('Remaining: ', len(data))
+
+hist, _ = np.histogram(data['throttle'], num_bins)
+plt.bar(center, hist, width=0.05)
+plt.plot((np.min(data['throttle']), np.max(data['throttle'])), (samples_per_bin, samples_per_bin))
+plt.show()
+
+print(data.iloc[1])
